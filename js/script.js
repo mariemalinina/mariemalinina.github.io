a11950ffce9bf38706708b428bee687daa56bc26
mariemalinina <33034189+mariemalinina@users.noreply.github.com>
2018-09-02 16:55:22 +0300

    Создаем публичную страницу

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var link = document.querySelector(".contact-link");
+var popup = document.querySelector(".feedback");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("feedback-show");
+});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("feedback-show");
+});
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("feedback-show")) {
+			popup.classList.remove("feedback-show");
+		}
+	}
+});
